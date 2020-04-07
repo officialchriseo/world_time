f57b9ce80b9d3a350e50305766ec8469aa3188ac
officicalchriseo <officialchriseo@gmail.com>
2020-04-07 22:38:54 +0100

    Initial commit

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('World Time'),
+        centerTitle: true,
+      ),
+      body: Text('Choose Location Screen'),
+    );
+  }
+}
